237cbc2ba70d67b9345c3e161f67d37a9fb24663
Crowdbotics <team@crowdbotics.com>
2023-11-17 20:10:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "loyalty-card-market-44650",
+  url: "https://loyalty-card-market-44650.botics.co",
+  api: "https://loyalty-card-market-44650.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
